42f4df6b04fb37dd11fb58ff67eaba69b94e86d8
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-08-07 16:58:42 -0300

    js starting

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let cell_1 = document.getElementById('cell-1');
+let cell_2 = document.getElementById('cell-2');
+let cell_3 = document.getElementById('cell-3');
+let cell_4 = document.getElementById('cell-4');
+let cell_5 = document.getElementById('cell-5');
+let cell_6 = document.getElementById('cell-6');
+let cell_7 = document.getElementById('cell-7');
+let cell_8 = document.getElementById('cell-8');
+let cell_9 = document.getElementById('cell-9');
+
+function verifyWinner() {
+    if 
+}
