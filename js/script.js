dbfca7ca99db84b68d75894203013d522867cebb
AnaClaraSM <itsanaclarasm.dev@gmail.com>
2024-08-08 09:33:38 -0300

    js defining variables and functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+//Buttons
+let start_btn = document.getElementById('start-btn');
+let about_btn = document.getElementById('about-btn');
+
+// Cells
 let cell_1 = document.getElementById('cell-1');
 let cell_2 = document.getElementById('cell-2');
 let cell_3 = document.getElementById('cell-3');
@@ -7,6 +12,19 @@ let cell_6 = document.getElementById('cell-6');
 let cell_7 = document.getElementById('cell-7');
 let cell_8 = document.getElementById('cell-8');
 let cell_9 = document.getElementById('cell-9');
+
+// Player -> X or O
+let player = "X";
+// End of Match Flag ->  0 == Match Off  |  1 == Match On
+let Match = "0";
+
+function clearBoard() {
+    
+}
+
+function positionMarkup() {
+
+}
 
 function verifyWinner() {
     if 
